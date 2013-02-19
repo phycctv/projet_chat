1603a24eb3d2143ccfd3160c73d513794ef3506c
phycctv <phycctv@gmail.com>
2013-02-19 10:28:45 +0100

    class

---
+++ src/projetclientserveur/Salon.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetclientserveur;
+
+/**
+ *
+ * @author zhangxi
+ */
+public class Salon {
+
+    private String identSalon;
+    
+}
---
+++ src/projetclientserveur/Utilisateur.java
@@ -0,0 +1,16 @@
+package projetclientserveur;
+
+import java.util.Date;
+/**
+ *
+ * @author zhangxi
+ */
+public class Utilisateur {
+
+    private String login;
+    private String mdp;
+    private String email;
+    private Date dateNais;
+    private boolean sexe;
+
+}
