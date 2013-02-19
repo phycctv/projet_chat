38e7ccfeb075a91ba884c71ffb0a9f2d94476c08
bodinjo <bodinjo@pc-dg-037-13.iut2.upmf-grenoble.fr>
2013-02-19 15:43:18 +0100

    inscription et utilisateur et salon.

--- src/projetclientserveur/Utilisateur.java
+++ src/projetclientserveur/Utilisateur.java
@@ -13,4 +13,52 @@ public class Utilisateur {
     private Date dateNais;
     private boolean sexe;
 
+    public Utilisateur(String login, String mdp, String email, Date dateNais, boolean sexe) {
+        this.login = login;
+        this.mdp = mdp;
+        this.email = email;
+        this.dateNais = dateNais;
+        this.sexe = sexe;
+    }
+
+    public Date getDateNais() {
+        return dateNais;
+    }
+
+    public void setDateNais(Date dateNais) {
+        this.dateNais = dateNais;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public boolean isSexe() {
+        return sexe;
+    }
+
+    public void setSexe(boolean sexe) {
+        this.sexe = sexe;
+    }
+
 }
